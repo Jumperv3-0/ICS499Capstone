1fe4f77c2ae3d09421957dc0075ef873642844e6
Jumperv3-0 <4gwebb@gmail.com>
2017-10-11 06:01:43 -0500

    inclueded functions.php

--- php/process_login.php
+++ php/process_login.php
@@ -1,8 +1,8 @@
 <?php
 	require_once 'db_connection.php';
+	require_once 'functions.php';
 
-	$instance = SqlManager::getInstance();
-	$conn = $instance->getConnection();
+	$db = SqlManager::getInstance();
 
 
 
