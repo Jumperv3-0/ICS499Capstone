db1bed6bfe07760b86d65589495f2076b1df0471
Jumperv3-0 <4gwebb@gmail.com>
2017-10-13 20:37:49 -0500

    New php pages

---
+++ php/getFooter.php
@@ -0,0 +1,10 @@
+<?php
+function getFooter(){
+	echo '		<div class="container text-center">
+			<h4>Contact Us</h4>
+			<div>
+				<div class="contact-info">Phone Number: 612 978 5555<span class="glyphicon glyphicon-earphone"></span></div>
+				<div class="contact-info">Email Address: GSalesStaff@Gsales.net<span class="glyphicon glyphicon-envelope"></span></div>
+			</div>
+		</div>'
+?>
