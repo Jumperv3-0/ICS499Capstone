6ce4c1d0abf71186b9fb7b4a332183db581ea79a
Jumperv3-0 <4gwebb@gmail.com>
2017-10-13 20:52:30 -0500

    fixed errors in getFooter method

--- php/getFooter.php
+++ php/getFooter.php
@@ -1,10 +1,11 @@
 <?php
-function getFooter(){
+function getFooter() {
 	echo '		<div class="container text-center">
 			<h4>Contact Us</h4>
 			<div>
 				<div class="contact-info">Phone Number: 612 978 5555<span class="glyphicon glyphicon-earphone"></span></div>
 				<div class="contact-info">Email Address: GSalesStaff@Gsales.net<span class="glyphicon glyphicon-envelope"></span></div>
 			</div>
-		</div>'
+		</div>';
+}
 ?>
