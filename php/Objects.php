0321d7611f329a5542c69059b4821485d310cc40
Jumperv3 <4gwebb@gmail.com>
2017-10-15 17:38:13 -0500

    adding pagebuilder object

--- php/Objects.php
+++ php/Objects.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'functions.php';
 class User {
   public function User($username, $email, $password, $city, $street, $zip, $phonenumber, $fname, $lname) {
 		$this->username = $username;
@@ -33,6 +34,191 @@ class GarageSale {
 		$this->items = $items;
 		$this->user = $user;
 	}
+}
+
+abstract class PageBuilder {
+	//$session_validator; // TODO: used to check if session if valid or needs to start
+
+	public function getHeader() {
+		$this->active = '';
+		$this->title = '';
+		$this->header = '';
+		$this->logged_in = false; // TODO: change to dynamic
+		$array = explode('/', sanitizeInput($_SERVER['PHP_SELF']));
+		$page_name = array_pop($array);
+		switch($page_name) {
+			case "index.php":
+				$this->active = 'index';
+				$this->header = $this->makeHeader($this->active, $this->title, $this->logged_in);
+				echo $this->header;
+				break;
+			case "Your sales.php":
+				$this->active = 'Your sales';
+				$this->header = $this->makeHeader($this->active, $this->title, $this->logged_in);
+				echo $this->header;
+				break;
+			case "items.php":
+				$this->active = 'items';
+				$this->header = $this->makeHeader($this->active, $this->title, $this->logged_in);
+				echo $this->header;
+				break;
+			case "createAccount.php":
+				$this->active = 'index';
+				$this->header = $this->makeHeader($this->active, $this->title, $this->logged_in);
+				echo $this->header;
+				break;
+			case "other sales.php":
+				$this->active = 'index';
+				$this->header = $this->makeHeader($this->active, $this->title, $this->logged_in);
+				echo $this->header;
+				break;
+			default:
+				$active = '';
+				$title = '';
+				$header = 'add Page';
+				echo $this->header;
+		}
+	}
+
+	public function getFooter() {
+
+	}
+
+	static function getTitle() {
+		$array = explode('/', sanitizeInput($_SERVER['PHP_SELF']));
+		$page_name = array_pop($array);
+		$array = array_reverse(explode('.',$page_name));
+		$title = ucfirst(array_pop($array));
+		echo $title . " | GSale";
+	}
+
+	protected function makeHeader($active, $title, $logged_in) {
+		$header = '';
+		if ($logged_in) {
+			$header = '
+						<nav class="navbar navbar-expand navbar-inverse navbar-fixed-top">
+							<div class="container-flex">
+								<div class="navbar-header">
+									<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#MyNavbar">
+										<span class="sr-only">Toggle navigation</span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+									</button>
+								<a class="navbar-brand" href="#"><img src="../img/garage_logo_final.png" /></a>
+								</div>
+								<div class="collapse navbar-collapse" id="MyNavbar">
+									<ul class="nav navbar-nav navbar float-left">
+									' . $this->getActive($active) . '
+									</ul>
+									<ul class="nav navbar-nav navbar-right">
+										<li>
+											<a href="#">
+												<form class="navbar-form navbar-right" role="search">
+													<div class="input-group">
+														<input id="search" type="text" class="form-control" placeholder="Search" name="search">
+														<div class="input-group-btn">
+															<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+														</div>
+													</div>
+												</form>
+											</a>
+										</li>
+										<li><a href="#"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>
+										<li><a href="createAccount.php">Create Account</a></li>
+									</ul>
+								</div>
+							</div>
+						</nav>';
+		} else {
+			$header = '
+						<nav class="navbar navbar-expand navbar-inverse navbar-fixed-top">
+							<div class="container-flex">
+								<div class="navbar-header">
+									<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#MyNavbar">
+										<span class="sr-only">Toggle navigation</span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+									</button>
+								<a class="navbar-brand" href="#"><img src="../img/garage_logo_final.png" /></a>
+								</div>
+								<div class="collapse navbar-collapse" id="MyNavbar">
+									<ul class="nav navbar-nav navbar float-left">
+									' . $this->getActive($active) . '
+									</ul>
+									<ul class="nav navbar-nav navbar-right">
+										<li>
+											<a href="#">
+												<form class="navbar-form navbar-right" role="search">
+													<div class="input-group">
+														<input id="search" type="text" class="form-control" placeholder="Search" name="search">
+														<div class="input-group-btn">
+															<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+														</div>
+													</div>
+												</form>
+											</a>
+										</li>
+										<li class="dropdown">
+<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">My Account <span class="caret"></span></a>
+										<ul class="dropdown-menu">
+											<li><a href="#">My Sales</a></li>
+											<li role="separator" class="divider"></li>
+											<li><a href="#">My Items</a></li>
+											<li role="separator" class="divider"></li>
+											<li><a href="#">Prefered Items</a></li>
+											<li role="separator" class="divider"></li>
+											<li><a href="#">One more separated link</a></li>
+										</ul>
+									</li>
+									<li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+									</ul>
+								</div>
+							</div>
+						</nav>';
+		}
+		return $header;
+	}
+
+	protected function getActive($active) {
+		$html = '';
+		$active_index = '';
+		switch($active) { // TODO: add other cases
+			case "index":
+				$active_index = 0;
+				break;
+			case "otherSales":
+				$active_index = 1;
+				break;
+			case "items":
+				$active_index = 2;
+				break;
+			default:
+				$active_index = 0;
+		}
+		$pages = array('index.php', 'otherSales.php', 'items.php');
+		echo "count is: " . count($pages);
+
+		for ($i = 0; $i < (count($pages) * 2); $i++ ) {
+			if ($i % 2 == 0 && $i == ($active_index * 2)) {
+				echo "active";
+				$html .= '<li class="active"><a href="' . $pages[$active_index] . '">' . $active . '</a></li>';
+			} else if ($i % 2 == 0){
+				echo "noormal";
+				$html .= '<li class=""><a href="' . $pages[$active_index] . '">$active</a></li>';
+			} else {
+				echo "divider";
+				$html .= '<li role="separator" class="divider"></li>';
+			}
+			return $html;
+		}
+	}
+
+}
+
+class IndexPage extends PageBuilder {
+
 }
 
 /**
--- php/index.php
+++ php/index.php
@@ -1,89 +1,96 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+	include_once 'Objects.php';
+?>
+	<!DOCTYPE html>
+	<html lang="en">
 
-<head>
-	<title>Bootstrap Website Tutorial</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="../css/styles.css">
-</head>
+	<head>
+		<title>
+			<?php IndexPage::getTitle() ?>
+		</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="../css/styles.css">
+	</head>
 
-<body>
-	<header>
-		<?php
-			include 'getNavBar.php';
-			getNavBar();
+	<body>
+		<header>
+			<?php
+			$pageBuilder = new IndexPage();
+			$pageBuilder->getHeader();
+//			include 'getNavBar.php';
+//			getNavBar();
 		?>
-	</header>
+		</header>
 
-	<div class="container">
-		<div class="col-sm-8 text-left">
-			<h1>Welcome</h1>
-			<p>Paragraph about us!</p>
-			<p>We At G=Sale have a simple goal. That goal is to help connect those wishing to sell their unneeded possessions to those who are looking for deals cutting out the middleman. A trade between neighbors. You might be hard on for cash. Or your looking to unburden yourself with unnecessary things you have gathered over the years. Regardless of your sepenstance you can create an account and set up your garage sale for free!
-			Or maybe you are looking for a old, no longer sold in stores item. or you just want to save some cash picking up a hand me down dresser. You don't need an account just simply check out our Sales page to find garage that are live near you. Or you can search by specific keyword if you already know what you're looking for.</p>
-			<hr>
-		</div>
-		<div class="col-sm-4 sidenav">
-			<div class="well text-center">
-				<h3>Login</h3>
-				<form action="<?php echo htmlspecialchars('process_login.php');?>" method="post">
-					<div class="input-group">
-						<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-						<input id="username" type="text" class="form-control" name="username" placeholder="Username">
-					</div>
-					<div class="input-group">
-						<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-						<input id="password" type="password" class="form-control" name="password" placeholder="Password">
-					</div>
-					<button name="submit" class="btn btn-default btn-login pull-right" type="submit">Login</button>
-					<span class="clearfix"></span>
-				</form>
+		<div class="container">
+			<div class="col-sm-8 text-left">
+				<h1>Welcome</h1>
+				<p>Paragraph about us!</p>
+				<p>We At G=Sale have a simple goal. That goal is to help connect those wishing to sell their unneeded possessions to those who are looking for deals cutting out the middleman. A trade between neighbors. You might be hard on for cash. Or your looking to unburden yourself with unnecessary things you have gathered over the years. Regardless of your sepenstance you can create an account and set up your garage sale for free! Or maybe you are looking for a old, no longer sold in stores item. or you just want to save some cash picking up a hand me down dresser. You don't need an account just simply check out our Sales page to find garage that are live near you. Or you can search by specific keyword if you already know what you're looking for.</p>
+				<hr>
 			</div>
-			<!--FUTURE: dynamic list of sales near your or searched for items?-->
+			<div class="col-sm-4 sidenav">
+				<div class="well text-center">
+					<h3>Login</h3>
+					<form action="<?php echo htmlspecialchars('process_login.php');?>" method="post">
+						<div class="input-group">
+							<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+							<input id="username" type="text" class="form-control" name="username" placeholder="Username">
+						</div>
+						<div class="input-group">
+							<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+							<input id="password" type="password" class="form-control" name="password" placeholder="Password">
+						</div>
+						<button name="submit" class="btn btn-default btn-login pull-right" type="submit">Login</button>
+						<span class="clearfix"></span>
+					</form>
+				</div>
+				<!--FUTURE: dynamic list of sales near your or searched for items?-->
+			</div>
 		</div>
-	</div>
-	<div class="container">
-		<div class="well">
-			<h3>Sales Near You</h3>
-			<ul class="list-group">
-				<li data-toggle="collapse" href="#collapse1" class="list-group-item">
-					<h5 class="collapse-header">Location:</h5>
-					<p class="collapse-header-text">700 7th Street East Saint Paul, MN 55106</p>
-					<span class="caret dropdown-caret pull-right"></span>
-					<p id="collapse1" class="collapse">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-				</li>
-				<li data-toggle="collapse" href="#collapse2" class="list-group-item">
-					<h5 class="collapse-header">Location:</h5>
-					<p class="collapse-header-text">700 7th Street East Saint Paul, MN 55106</p>
-					<span class="caret dropdown-caret pull-right"></span>
-					<p id="collapse2" class="collapse">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-				</li>
-				<li data-toggle="collapse" href="#collapse3" class="list-group-item">
-					<h5 class="collapse-header">Location:</h5>
-					<p class="collapse-header-text">700 7th Street East Saint Paul, MN 55106</p>
-					<span class="caret dropdown-caret pull-right"></span>
-					<p id="collapse3" class="collapse">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-				</li>
-				<li data-toggle="collapse" href="#collapse4" class="list-group-item">
-					<h5 class="collapse-header">Location:</h5>
-					<p class="collapse-header-text">700 7th Street East Saint Paul, MN 55106</p>
-					<span class="caret dropdown-caret pull-right"></span>
-					<p id="collapse4" class="collapse">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-				</li>
-			</ul>
+		<div class="container">
+			<div class="well">
+				<h3>Sales Near You</h3>
+				<ul class="list-group">
+					<li data-toggle="collapse" href="#collapse1" class="list-group-item">
+						<h5 class="collapse-header">Location:</h5>
+						<p class="collapse-header-text">700 7th Street East Saint Paul, MN 55106</p>
+						<span class="caret dropdown-caret pull-right"></span>
+						<p id="collapse1" class="collapse">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					</li>
+					<li data-toggle="collapse" href="#collapse2" class="list-group-item">
+						<h5 class="collapse-header">Location:</h5>
+						<p class="collapse-header-text">700 7th Street East Saint Paul, MN 55106</p>
+						<span class="caret dropdown-caret pull-right"></span>
+						<p id="collapse2" class="collapse">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					</li>
+					<li data-toggle="collapse" href="#collapse3" class="list-group-item">
+						<h5 class="collapse-header">Location:</h5>
+						<p class="collapse-header-text">700 7th Street East Saint Paul, MN 55106</p>
+						<span class="caret dropdown-caret pull-right"></span>
+						<p id="collapse3" class="collapse">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					</li>
+					<li data-toggle="collapse" href="#collapse4" class="list-group-item">
+						<h5 class="collapse-header">Location:</h5>
+						<p class="collapse-header-text">700 7th Street East Saint Paul, MN 55106</p>
+						<span class="caret dropdown-caret pull-right"></span>
+						<p id="collapse4" class="collapse">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					</li>
+				</ul>
+			</div>
 		</div>
-	</div>
 
-	<footer>
-		<?php
+		<footer>
+			<?php
 			include 'getFooter.php';
 			getFooter();
 		?>
-	</footer>
-</body>
-</html>
+		</footer>
+	</body>
+
+	</html>
