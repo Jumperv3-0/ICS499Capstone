6bcb01ca6a0b2a36e50f86a485416c64845952ba
Jumperv3-0 <4gwebb@gmail.com>
2017-10-16 06:23:14 -0500

    fixed session creation

--- php/init.php
+++ php/init.php
@@ -1,15 +1,25 @@
 <?php
 	require_once 'db_config.php';
-	$session_name = 'user'; // Set a custom session name
-	$secure = SECURE;
-	$httpOnly = true;
-
-	if (ini_set('session.use_only_cookies', 1) === FALSE) {
-		header("Location: 404.php?error=Please enable cookies");
-		exit();
-	}
-	$cookieParams = session_get_cookie_params();
-	session_set_cookie_params($cookieParams['lifetime'], $cookieParams['path'], $cookieParams['domain'], $secure, $httpOnly);
-	session_name($session_name);
+	$GLOBALS['config'] = array(
+        'remember' => array(
+            'cookie_name' => 'hash',
+            'cookie_expire' => 604800
+        ),
+        'session' => array(
+            'session_name' => 'user',
+            'token_name' => 'token'
+        )
+    );
+//    $session_name = 'user'; // Set a custom session name
+//	$secure = SECURE;
+//	$httpOnly = true;
+//
+//	if (ini_set('session.use_only_cookies', 1) === FALSE) {
+//		header("Location: 404.php?error=Please enable cookies");
+//		exit();
+//	}
+//	$cookieParams = session_get_cookie_params();
+//	session_set_cookie_params($cookieParams['lifetime'], $cookieParams['path'], $cookieParams['domain'], $secure, $httpOnly);
+//	session_name($session_name);
 	session_start();
-	session_regenerate_id();
+//	session_regenerate_id();
