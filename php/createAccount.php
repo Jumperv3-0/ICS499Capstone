5cfaba59609525436a7faaf17a43d66687a732f1
Jumperv3-0 <4gwebb@gmail.com>
2017-10-16 06:25:08 -0500

    added functionality to create account

--- php/createAccount.php
+++ php/createAccount.php
@@ -1,225 +1,162 @@
-createAccount.php DETAILS ACTIVITY Yesterday Thu 2:22 PM D David Gruenberg edited an item HTML createAccount.php Thu 1:51 PM D David Gruenberg uploaded an item HTML createAccount.php No recorded activity before October 12, 2017 Changed to grid view. All selections cleared All selections cleared All selections cleared
+<?php
+	require_once 'init.php';
+    require_once 'Objects.php';
+    require_once 'functions.php';
+?>
+	<!DOCTYPE html>
+	<html lang="en">
 
-<!DOCTYPE html>
-<html lang="en">
+	<head>
+		<title><?php PageBuilder::getTitle() ?></title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="../css/styles.css">
+	</head>
 
-<head>
-	<title>Bootstrap Website Tutorial</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="./css/styles.css">
-	<style>
-		body {
-			padding-top: 8em;
-		}
+	<body>
+		<header>
+            <?php
+                $pageBuilder = new RegisterPage();
+                $pageBuilder->getHeader();
+            ?>
+		</header>
+		<div class="container">
+			<?php
 
-		.separator {
-			border-bottom: #526747 5px solid;
-		}
+				if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+					if (isset($_POST['submit'])) {
+                        if (Token::check(sanitizeInput($_POST['token']))) {
+                            $validator = new Validation();
+                            $validation = $validator->check($_POST, array(
+                                'username' => array(
+                                    'required' => true,
+                                    'min' => 4,
+                                    'max' => 22,
+                                    'unique' => 'users'
+                                ),
+                                'password' => array(
+                                    'required' => true,
+                                    'min' => 6,
+                                    'max' => 22
+                                ),
+                                'password_again' => array(
+                                    'required' => true,
+                                    'min' => 6,
+                                    'max' => 22,
+                                    'matches' => 'password'
+                                ),
+                                'fname' => array(
+                                    'required' => true,
+                                    'min' => 2,
+                                    'max' => 30
+                                ),
+                                'lname' => array(
+                                    'required' => true,
+                                    'min' => 2,
+                                    'max' => 30
+                                ),
+                                'phone' => array(
+                                    'required' => true,
+                                    'min' => 10,
+                                    'max' => 13,
+                                    'phone' => true
+                                ),
+                                'email' => array(
+                                    'required' => true,
+                                    'min' => 2,
+                                    'max' => 50,
+                                    'email' => true
+                                ),
+                                'address' => array(
+                                    'required' => true,
+                                    'min' => 2,
+                                    'max' => 120,
+                                    'address' => true
+                                ),
+                            ));
 
-		.btn-login {
-			color: #fff;
-			background-color: #526747;
-			border-color: #ccc;
-			padding: .5em 3em;
-		}
+                            if ($validation->passed()) { // create account for user
+                                $user = new User();
+                                $place = new Place();
+                                try {
+                                     /*
+                                    // TODO: Break $_POST['address'] into address, city, state, zip, country
+                                    $place->create(array(
 
-		.btn-login:hover {
-			color: #fff;
-			background-color: #234214;
-			border-color: #ccc;
-		}
+                                    ));
+                                     $location_fk = '' // TODO: get place_id from query above
 
-		.navbar {
-			margin-bottom: 0;
-			border-radius: 0;
-			background-color: #234214;
-			color: #fff;
-			padding: .5% 0;
-			font-size: 1.1em;
-			border-left: none;
-			border-right: none;
-			border-bottom: #526747 5px solid;
-		}
+                                    // TODO: implement create place before uncommenting
+                                    $user->create(array(
+                                        sanitizeInput($_POST['username']),
+                                        password_hash(sanitizeInput($_POST['password']), PASSWORD_DEFAULT),
+                                        sanitizeInput($_POST['fname']),
+                                        sanitizeInput($_POST['lname']),
+                                        sanitizeInput($_POST['email']),
+                                        sanitizeInput($_POST['phone']),
+                                        $location_fk));
+                                        */
+                                } catch(Exception $e) {
+                                    die ($e->getMessage());
+                                }
+                                Session::flash('success', 'You registered successfully!');
+                                Redirect::page('index.php');
+                            } else { // Display errors
+                               foreach($validation->getErrors() as $error) {
+                                   echo $error . "<br>";
+                               }
+                            }
+                        }
 
-		.navbar-inverse .navbar-collapse {
-			border-color: #526747;
-		}
-
-		.navbar-brand {
-			float: left;
-			/*min-height: 55px;*/
-			padding: 0 15px 5px;
-		}
-
-		.navbar-inverse .navbar-nav .active a,
-		.navbar-inverse .navbar-nav .active a:focus,
-		.navbar-inverse .navbar-nav .active a:hover {
-			color: #fff;
-			background-color: #234214;
-		}
-
-		.navbar-brand img {
-			width: 5.8em;
-		}
-
-		.navbar-inverse .navbar-nav>.open>a,
-		.navbar-inverse .navbar-nav>.open>a:focus,
-		.navbar-inverse .navbar-nav>.open>a:hover {
-			color: #fff;
-			background-color: #234214;
-		}
-
-		.nav {
-			margin-bottom: 0;
-			border-radius: 0;
-			background-color: #234214;
-			color: #fff;
-			padding: .5% 0;
-			font-size: 1.1em;
-			border: none;
-		}
-
-		.navbar-header {
-			min-height: 3.8em;
-		}
-
-		.navbar-inverse .navbar-toggle:focus,
-		.navbar-inverse .navbar-toggle:hover {
-			background-color: #526747;
-		}
-
-		.navbar-inverse .navbar-toggle {
-			border-color: #fff;
-		}
-
-		.navbar-form {
-			padding: 0;
-			margin-top: -.4em;
-		}
-
-		.container-flex {
-			max-width: 1200px !important;
-			margin: auto;
-		}
-
-		footer {
-			color: #fff;
-			background-color: #234214;
-			border-color: #ccc;
-		}
-
-		.collapse-header {
-			display: inline;
-			font-weight: bold;
-			color: #234214;
-		}
-
-		.collapse-header-text {
-			display: inline
-		}
-
-		.dropdown-caret {
-			color: #234214;
-			margin-top: .5em;
-		}
-
-	</style>
-</head>
-
-<body>
-	<header>
-		<nav class="navbar navbar-expand navbar-inverse navbar-fixed-top">
-			<div class="container-flex">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#MyNavbar">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-					<a class="navbar-brand" href="#"><img src="img/garage_logo_final.png" /></a>
+					}
+				}
+			?>
+			<form action="<?php echo sanitizeInput($_SERVER['PHP_SELF']); ?>" method="post">
+				<div class="form-group">
+					<label for="username">Username:</label>
+					<input type="text" class="form-control" id="username" name="username" value="<?php echo (isset($_POST['username']) ? sanitizeInput($_POST['username']) : ""); ?>">
 				</div>
-				<div class="collapse navbar-collapse" id="MyNavbar">
-					<ul class="nav navbar-nav navbar float-left">
-						<li class="active">
-							<a href="#">Home</a>
-						</li>
-						<li role="separator" class="divider"></li>
-						<li><a href="">Sales</a></li>
-						<li role="separator" class="divider"></li>
-						<li><a href="">Items</a></li>
-						<li role="separator" class="divider"></li>
-					</ul>
-					<ul class="nav navbar-nav navbar-right">
-						<li>
-							<a href="#">
-								<form class="navbar-form navbar-right" role="search">
-									<div class="input-group">
-										<input id="search" type="text" class="form-control" placeholder="Search" name="search">
-										<div class="input-group-btn">
-											<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-										</div>
-									</div>
-								</form>
-							</a>
-						</li>
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">My Account <span class="caret"></span></a>
-							<ul class="dropdown-menu">
-								<li><a href="#">My Sales</a></li>
-								<li role="separator" class="divider"></li>
-								<li><a href="#">My Items</a></li>
-								<li role="separator" class="divider"></li>
-								<li><a href="#">Prefered Items</a></li>
-								<li role="separator" class="divider"></li>
-								<li><a href="#">One more separated link</a></li>
-							</ul>
-						</li>
-						<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-					</ul>
+				<div class="form-group">
+					<label for="password">Password:</label>
+					<input type="password" class="form-control" id="password" name="password" >
 				</div>
-			</div>
-		</nav>
-	</header>
-	<div class="container">
-		<form>
-			<div class="form-group">
-				<label for="fname">First Name:</label>
-				<input type="text" class="form-control" id="fname">
-			</div>
-			<div class="form-group">
-				<label for="lname">Last Name:</label>
-				<input type="text" class="form-control" id="lname">
-			</div>
-			<div class="form-group">
-				<label for="phone">Phone:</label>
-				<input type="tel" class="form-control" id="phone">
-			</div>
-			<div class="form-group">
-				<label for="email">Email:</label>
-				<input type="email" class="form-control" id="email">
-			</div>
-			<div class="form-group">
-				<label for="address">Address:</label>
-				<input type="text" class="form-control" id="address">
-			</div>
-			<div class="form-group">
-				<label for="password">Password:</label>
-				<input type="text" class="form-control" id="password">
-			</div>
-			<div class="form-group">
-				<label for="user name">User Name:</label>
-				<input type="text" class="form-control" id="user name">
-			</div>
+				<div class="form-group">
+					<label for="password_again">Reenter Password:</label>
+					<input type="password" class="form-control" id="password_again" name="password_again">
+				</div>
+				<div class="form-group">
+					<label for="fname">First Name:</label>
+					<input type="text" class="form-control" id="fname" name="fname" value="<?php echo (isset($_POST['fname']) ? sanitizeInput($_POST['fname']) : ""); ?>">
+				</div>
+				<div class="form-group">
+					<label for="lname">Last Name:</label>
+					<input type="text" class="form-control" id="lname" name="lname" value="<?php echo (isset($_POST['lname']) ? sanitizeInput($_POST['lname']) : ""); ?>">
+				</div>
+				<div class="form-group">
+					<label for="phone">Phone:</label>
+					<input type="tel" class="form-control" id="phone" name="phone" value="<?php echo (isset($_POST['phone']) ? sanitizeInput($_POST['phone']) : ""); ?>">
+				</div>
+				<div class="form-group">
+					<label for="email">Email:</label>
+					<input type="email" class="form-control" id="email" name="email" value="<?php echo (isset($_POST['email']) ? sanitizeInput($_POST['email']) : ""); ?>">
+				</div>
+				<div class="form-group">
+					<label for="address">Address:</label>
+					<input type="text" class="form-control" id="address" name="address" value="<?php echo (isset($_POST['address']) ? sanitizeInput($_POST['address']) : ""); ?>">
+				</div>
+				<input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
+				<button type="submit" class="btn btn-default" name="submit">Submit</button>
+			</form>
+		</div>
+		<footer>
+		    <?php
+                PageBuilder::getFooter();
+            ?>
+		</footer>
+	</body>
 
-			<button type="submit" class="btn btn-default">Submit</button>
-		</form>
-	</div>
-</body>
-
-</html>
+	</html>
