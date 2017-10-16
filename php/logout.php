57c1722b8eae5780d1c3bf1904beeb3a349c7c8f
Jumperv3-0 <4gwebb@gmail.com>
2017-10-16 09:31:25 -0500

    Added new file for logout

---
+++ php/logout.php
@@ -0,0 +1,9 @@
+<?php
+    require_once 'init.php';
+    require_once 'Objects.php';
+
+    $user = new User();
+    $user->logout();
+    Session::flash('logout', 'You have been logged out!');
+    Redirect::page('index.php');
+?>
