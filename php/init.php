8dca8f5235edf40ce23a8fc42abe9e1e39c9ddc3
Jumperv3-0 <4gwebb@gmail.com>
2017-10-15 19:43:25 -0500

    added file

---
+++ php/init.php
@@ -0,0 +1,15 @@
+<?php
+	require_once 'db_config.php';
+	$session_name = 'user'; // Set a custom session name
+	$secure = SECURE;
+	$httpOnly = true;
+
+	if (ini_set('session.use_only_cookies', 1) === FALSE) {
+		header("Location: 404.php?error=Please enable cookies");
+		exit();
+	}
+	$cookieParams = session_get_cookie_params();
+	session_set_cookie_params($cookieParams['lifetime'], $cookieParams['path'], $cookieParams['domain'], $secure, $httpOnly);
+	session_name($session_name);
+	session_start();
+	session_regenerate_id();
