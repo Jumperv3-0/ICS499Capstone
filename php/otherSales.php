eacb6d3266c4d581c223f34432a0ee363d21a8f5
Jumperv3-0 <4gwebb@gmail.com>
2017-10-16 08:16:29 -0500

    now uses pagebuilder

--- php/otherSales.php
+++ php/otherSales.php
@@ -1,140 +1,36 @@
+<?php
+    require_once 'init.php';
+    require_once 'Objects.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-	<title>Bootstrap Website Tutorial</title>
+	<title><?php PageBuilder::getTitle() ?></title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="./css/styles.css">
+	<link rel="stylesheet" type="text/css" href="../css/styles.css">
 </head>
 
 <body>
 	<header>
 		<?php
-			include 'getNavBar.php';
-			getNavBar();
-		?>
+            $pageBuilder = new SalesPage();
+            $pageBuilder->getHeader();
+        ?>
 	</header>
-	<style>
-	body {
-	padding-top: 8em;
-}
-
-.separator {
-	border-bottom: #526747 5px solid;
-}
-
-.btn-login {
-	color: #fff;
-	background-color: #526747;
-	border-color: #ccc;
-	padding: .5em 3em;
-}
-
-.btn-login:hover {
-	color: #fff;
-	background-color: #234214;
-	border-color: #ccc;
-}
-
-.navbar {
-	margin-bottom: 0;
-	border-radius: 0;
-	background-color: #234214;
-	color: #fff;
-	padding: .5% 0;
-	font-size: 1.1em;
-	border-left: none;
-	border-right: none;
-	border-bottom: #526747 5px solid;
-}
-
-.navbar-inverse .navbar-collapse {
-	border-color: #526747;
-}
-
-.navbar-brand {
-	float: left;
-	/*min-height: 55px;*/
-	padding: 0 15px 5px;
-}
-
-.navbar-inverse .navbar-nav .active a,
-.navbar-inverse .navbar-nav .active a:focus,
-.navbar-inverse .navbar-nav .active a:hover {
-	color: #fff;
-	background-color: #234214;
-}
-
-.navbar-brand img {
-	width: 5.8em;
-}
-
-.navbar-inverse .navbar-nav>.open>a,
-.navbar-inverse .navbar-nav>.open>a:focus,
-.navbar-inverse .navbar-nav>.open>a:hover {
-	color: #fff;
-	background-color: #234214;
-}
-
-.nav {
-	margin-bottom: 0;
-	border-radius: 0;
-	background-color: #234214;
-	color: #fff;
-	padding: .5% 0;
-	font-size: 1.1em;
-	border: none;
-}
-
-.navbar-header {
-	min-height: 3.8em;
-}
-
-.navbar-inverse .navbar-toggle:focus,
-.navbar-inverse .navbar-toggle:hover {
-	background-color: #526747;
-}
-
-.navbar-inverse .navbar-toggle {
-	border-color: #fff;
-}
-
-.navbar-form {
-	padding: 0;
-	margin-top: -.4em;
-}
-
-.container-flex {
-	max-width: 1200px !important;
-	margin: auto;
-}
-
-footer {
-	color: #fff;
-	background-color: #234214;
-	border-color: #ccc;
-}
-
-.collapse-header {
-	display: inline;
-	font-weight: bold;
-	color: #234214;
-}
-
-.collapse-header-text {
-	display: inline
-}
-
-.dropdown-caret {
-	color: #234214;
-	margin-top: .5em;
-}
-</style>
+	<?php
+        $user = new User();
+        var_dump($user);
+        if ($user->isLoggedIn()) {
+            echo "logged in still!";
+        }
+    ?>
 <div class="container">
 		<div class="well">
 			<h3>Sales Near You</h3>
@@ -168,9 +64,8 @@ footer {
 	</div>
 	<footer>
 		<?php
-			include 'getFooter.php';
-			getFooter();
-		?>
+            PageBuilder::getFooter();
+        ?>
 	</footer>
 </body>
 </html>
