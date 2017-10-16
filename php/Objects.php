b02d34f456149ac26583df0c593bef03c0c90d48
PlayerPi <Henagata@gmail.com>
2017-10-16 13:53:45 -0500

    Update Objects.php

--- php/Objects.php
+++ php/Objects.php
@@ -299,87 +299,87 @@ CONTENT;
 		$header = '';
 		if (!$logged_in) {
 			$header = '
-						<nav class="navbar navbar-expand navbar-inverse navbar-fixed-top">
-							<div class="container">
-								<div class="navbar-header">
-									<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#MyNavbar">
-										<span class="sr-only">Toggle navigation</span>
-										<span class="icon-bar"></span>
-										<span class="icon-bar"></span>
-										<span class="icon-bar"></span>
-									</button>
-								<a class="navbar-brand" href="index.php"><img src="../img/garage_logo_final.png" /></a>
-								</div>
-								<div class="collapse navbar-collapse" id="MyNavbar">
-									<ul class="nav navbar-nav navbar float-left">
-									' . $this->getActive($active) . '
-									</ul>
-									<ul class="nav navbar-nav navbar-right">
-										<li>
-											<a href="#">
-												<form class="navbar-form navbar-right" role="search">
-													<div class="input-group">
-														<input id="search" type="text" class="form-control" placeholder="Search" name="search">
-														<div class="input-group-btn">
-															<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-														</div>
-													</div>
-												</form>
-											</a>
-										</li>
-										<li><a href="#"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>
-										<li><a href="createAccount.php">Create Account</a></li>
-									</ul>
-								</div>
-							</div>
-						</nav>';
-		} else {
-			$header = '
-						<nav class="navbar navbar-expand navbar-inverse navbar-fixed-top">
-							<div class="container">
-								<div class="navbar-header">
-									<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#MyNavbar">
-										<span class="sr-only">Toggle navigation</span>
-										<span class="icon-bar"></span>
-										<span class="icon-bar"></span>
-										<span class="icon-bar"></span>
-									</button>
-								<a class="navbar-brand" href="#"><img src="../img/garage_logo_final.png" /></a>
-								</div>
-								<div class="collapse navbar-collapse" id="MyNavbar">
-									<ul class="nav navbar-nav navbar float-left">
-									' . $this->getActive($active) . '
-									</ul>
-									<ul class="nav navbar-nav navbar-right">
-										<li>
-											<a href="#">
-												<form class="navbar-form navbar-right" role="search">
-													<div class="input-group">
-														<input id="search" type="text" class="form-control" placeholder="Search" name="search">
-														<div class="input-group-btn">
-															<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-														</div>
-													</div>
-												</form>
-											</a>
-										</li>
-										<li class="dropdown">
-<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">My Account <span class="caret"></span></a>
-										<ul class="dropdown-menu">
-											<li><a href="yourSales.php">My Sales</a></li>
-											<li role="separator" class="divider"></li>
-											<li><a href="#">My Items</a></li>
-											<li role="separator" class="divider"></li>
-											<li><a href="#">Prefered Items</a></li>
-											<li role="separator" class="divider"></li>
-											<li><a href="#">One more separated link</a></li>
-										</ul>
-									</li>
-									<li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
-									</ul>
-								</div>
-							</div>
-						</nav>';
+				<nav class="navbar navbar-expand navbar-inverse navbar-fixed-top">
+					<div class="container">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#MyNavbar">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						<a class="navbar-brand" href="index.php"><img src="../img/garage_logo_final.png" /></a>
+						</div>
+						<div class="collapse navbar-collapse" id="MyNavbar">
+							<ul class="nav navbar-nav navbar float-left">
+							' . $this->getActive($active) . '
+							</ul>
+							<ul class="nav navbar-nav navbar-right">
+								<li>
+									<a href="#">
+										<form class="navbar-form navbar-right" role="search">
+											<div class="input-group">
+												<input id="search" type="text" class="form-control" placeholder="Search" name="search">
+												<div class="input-group-btn">
+													<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+												</div>
+											</div>
+										</form>
+									</a>
+								</li>
+								<li><a href="#"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>
+								<li><a href="createAccount.php">Create Account</a></li>
+							</ul>
+						</div>
+					</div>
+				</nav>';
+	} else {
+	$header = '
+				<nav class="navbar navbar-expand navbar-inverse navbar-fixed-top">
+					<div class="container">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#MyNavbar">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						<a class="navbar-brand" href="#"><img src="../img/garage_logo_final.png" /></a>
+						</div>
+						<div class="collapse navbar-collapse" id="MyNavbar">
+							<ul class="nav navbar-nav navbar float-left">
+							' . $this->getActive($active) . '
+							</ul>
+							<ul class="nav navbar-nav navbar-right">
+								<li>
+									<a href="#">
+										<form class="navbar-form navbar-right" role="search">
+											<div class="input-group">
+												<input id="search" type="text" class="form-control" placeholder="Search" name="search">
+												<div class="input-group-btn">
+													<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+												</div>
+											</div>
+										</form>
+									</a>
+								</li>
+								<li class="dropdown">
+	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">My Account <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="yourSales.php">My Sales</a></li>
+									<li role="separator" class="divider"></li>
+									<li><a href="#">My Items</a></li>
+									<li role="separator" class="divider"></li>
+									<li><a href="#">Prefered Items</a></li>
+									<li role="separator" class="divider"></li>
+									<li><a href="#">One more separated link</a></li>
+								</ul>
+							</li>
+							<li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+							</ul>
+						</div>
+					</div>
+				</nav>';
 		}
 		return $header;
 	}
