771761beb6b133e21c3e8bb93ff44ce1e073b7bc
Jumperv3-0 <4gwebb@gmail.com>
2017-10-17 17:17:08 -0500

    Will now login user if they check remember me and they have a cookie that matches the one stored in our db

--- php/init.php
+++ php/init.php
@@ -1,7 +1,10 @@
 <?php
 	require_once 'db_config.php';
-    session_start();
-    session_regenerate_id();
+  require_once 'Objects.php';
+
+		ob_start();
+		session_start();
+		session_regenerate_id();
 	$GLOBALS['config'] = array(
         'remember' => array(
             'cookie_name' => 'hash',
@@ -12,3 +15,12 @@
             'token_name' => 'token'
         )
     );
+  if (Cookie::exists($GLOBALS['config']['remember']['cookie_name']) && !Session::exists($GLOBALS['config']['session']['session_name'])) {
+    $hash = Cookie::get($GLOBALS['config']['remember']['cookie_name']);
+    $hashCheck = SqlManager::getInstance()->query("SELECT * FROM login_attempts WHERE session_id = ?", array($hash));
+
+    if ($hashCheck->getCount()) {
+      $user = new User($hashCheck->getResult()[0]->user_fk_id);
+      $user->login();
+    }
+  }
