d0563fc9bb3e41ffdff0c519bb6bd5cd26c2b5b9
Jumperv3 <4gwebb@gmail.com>
2017-10-22 19:43:48 -0500

    google api second example

---
+++ php/object_test.php
@@ -0,0 +1,6 @@
+<?php
+	include_once 'init.php';
+	include_once 'Objects.php';
+	$user = new User();
+	//$user->edit(array("Jumperv3", "$2y$10$6TJJd59Fv3CyYo.secbNMu4/cow88wHvRDgn/bAPOnByW2SoD2./i", "Sam", "Bromson", "4gwebb@gmail.com"),4);
+?>
