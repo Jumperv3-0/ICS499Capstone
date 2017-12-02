b6e7e62a35d758bca54f66dc799f9a2d33f5b4b1
Jumperv3-0 <4gwebb@gmail.com>
2017-12-02 08:50:20 -0600

    fixed formating dates

--- php/functions.php
+++ php/functions.php
@@ -1,54 +1,59 @@
 <?php
 
-	function sec_session_start() {
-			$session_name = 'sec_session_id';   // Set a custom session name
-			$secure = SECURE;
-			// This stops JavaScript being able to access the session id.
-			$httponly = true;
-			// Forces sessions to only use cookies.
-			if (ini_set('session.use_only_cookies', 1) === FALSE) {
-					header("Location: ../error.php?err=Could not initiate a safe session (ini_set)");
-					exit();
-			}
-			// Gets current cookies params.
-			$cookieParams = session_get_cookie_params();
-			session_set_cookie_params($cookieParams["lifetime"], $cookieParams["path"], $cookieParams["domain"], $secure, $httponly);
-			// Sets the session name to the one set above.
-			session_name($session_name);
-			session_start();            // Start the PHP session
-			session_regenerate_id();    // regenerated the session, delete the old one.
-	}
+function sec_session_start() {
+  $session_name = 'sec_session_id';   // Set a custom session name
+  $secure = SECURE;
+  // This stops JavaScript being able to access the session id.
+  $httponly = true;
+  // Forces sessions to only use cookies.
+  if (ini_set('session.use_only_cookies', 1) === FALSE) {
+    header("Location: ../error.php?err=Could not initiate a safe session (ini_set)");
+    exit();
+  }
+  // Gets current cookies params.
+  $cookieParams = session_get_cookie_params();
+  session_set_cookie_params($cookieParams["lifetime"], $cookieParams["path"], $cookieParams["domain"], $secure, $httponly);
+  // Sets the session name to the one set above.
+  session_name($session_name);
+  session_start();            // Start the PHP session
+  session_regenerate_id();    // regenerated the session, delete the old one.
+}
 
-	function formatDates() {
-		$string = '';
-		for($i = 0; $i < count($_POST['date']); $i++) {
-			$string .= sanitizeInput($_POST['startTime'][$i]) . "-" . sanitizeInput($_POST['endTime'][$i]) . "-" . sanitizeInput($_POST['date'][$i]) . ",";
-		}
-		return $string;
-	}
+function formatDates() {
+  $string = '';
+  for($i = 0; $i < count($_POST['date']); $i++) {
+    $date = explode('/', sanitizeInput($_POST['date'][$i]));
+    $date = $date[2] . '/' . $date[0] . '/' . $date[1];
+    if ($i + 1 == count($_POST['date'])) {
+      $string .= sanitizeInput($_POST['startTime'][$i]) . "-" . sanitizeInput($_POST['endTime'][$i]) . "-" . $date;
+    }
+    $string .= sanitizeInput($_POST['startTime'][$i]) . "-" . sanitizeInput($_POST['endTime'][$i]) . "-" . $date . ",";
+  }
+  return $string;
+}
 
 
-	/**
+/**
 	 * Sanitizes input to make sure it is safe to use
 	 * @param  string $input to be sanitized
 	 * @return string safe to use input
 	 */
-	function sanitizeInput($input) {
-		if (is_array($input)) {
-			for($i = 0; $i < count($input); $i++) {
-				$data = $input[$i];
-				$data = trim($data);
-				$data = stripslashes($data);
-				$data = htmlspecialchars($data);
-				$input[$i] = $data;
-			}
-			return $input;
-		} else {
-			$data = $input;
-			$data = trim($data);
-			$data = stripslashes($data);
-			$data = htmlspecialchars($data);
-			return $data;
-		}
-	}
+function sanitizeInput($input) {
+  if (is_array($input)) {
+    for($i = 0; $i < count($input); $i++) {
+      $data = $input[$i];
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      $input[$i] = $data;
+    }
+    return $input;
+  } else {
+    $data = $input;
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+}
 
