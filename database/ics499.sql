ae5043c64de217c146deaf2c05c5e28b6b7f0c90
Jumperv3-0 <4gwebb@gmail.com>
2017-12-04 16:39:59 -0600

    new db

--- database/ics499.sql
+++ database/ics499.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 06, 2017 at 06:44 PM
+-- Generation Time: Dec 04, 2017 at 11:28 PM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.9
 
@@ -41,8 +41,11 @@ CREATE TABLE `garage_sales` (
 --
 
 INSERT INTO `garage_sales` (`gsale_id`, `sale_name`, `image_url`, `description`, `dates`) VALUES
-(1, 'First Test', 'first_sale.png', 'This is our first test of a garage sale. It is not a real one but a fake one. Why are you still reading this text its pointless. Did you want some useful information lol, look elsewhere.', '10:00-15:00-11/4/2017,10:00-15:30-11/5/2017'),
-(3, '2nd sale', '../uploads/62e50867dfe9e8f696edf5f6f19e8d88.png', 'This is a second sale for our db', '13:00-15:00-2017-11-06,');
+(1, 'First Test', '../uploads/76afface4080c9c8e905619aca932875.jpg', 'This is a description for our first sale. It does not contain any useful information so I don\'t know why you are still reading this.', '13:00-15:00-2017/12/12,13:00-15:00-2017/12/13'),
+(3, '2nd sale', '../uploads/62e50867dfe9e8f696edf5f6f19e8d88.png', 'This is a second sale for our db', '13:00-15:00-2017/12/12'),
+(4, '3rd Sale', 'https://dummyimage.com/200x200/333/fff.png&text=No', 'We have some baby clothes along with some old vhs tapes.', '10:00-17:00-2017/12/30'),
+(5, '5 Sale', '../uploads/4d9b2b7e3e93f4e972e19797f71879c9.jpg', 'Some sample text to fill space', '10:00-15:00-2017/12/05'),
+(6, '6th sale', '../uploads/784d5ed8494895965d9d7f851ce9d952.jpg', 'Some text for the 6th sale', '10:00-15:00-2017/12/05');
 
 -- --------------------------------------------------------
 
@@ -60,7 +63,7 @@ CREATE TABLE `garage_sales_items` (
 --
 
 INSERT INTO `garage_sales_items` (`gsale_fk_id`, `item_fk_id`) VALUES
-(1, 1);
+(1, 3);
 
 -- --------------------------------------------------------
 
@@ -78,9 +81,11 @@ CREATE TABLE `garage_sales_phones` (
 --
 
 INSERT INTO `garage_sales_phones` (`garage_sale_fk_id`, `phone_fk_id`) VALUES
-(1, 1),
-(1, 1),
-(1, 1);
+(1, 2),
+(3, 1),
+(4, 3),
+(5, 4),
+(6, 5);
 
 -- --------------------------------------------------------
 
@@ -92,6 +97,17 @@ CREATE TABLE `garage_sales_places` (
   `garage_sale_fk_id` int(22) NOT NULL,
   `place_fk_id` int(22) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `garage_sales_places`
+--
+
+INSERT INTO `garage_sales_places` (`garage_sale_fk_id`, `place_fk_id`) VALUES
+(1, 6),
+(3, 12),
+(4, 16),
+(5, 17),
+(6, 18);
 
 -- --------------------------------------------------------
 
@@ -109,7 +125,11 @@ CREATE TABLE `garage_sales_users` (
 --
 
 INSERT INTO `garage_sales_users` (`user_fk_id`, `garage_sales_fk_id`) VALUES
-(1, 1);
+(1, 1),
+(1, 3),
+(1, 4),
+(1, 5),
+(1, 6);
 
 -- --------------------------------------------------------
 
@@ -137,7 +157,7 @@ CREATE TABLE `items` (
   `item_id` int(22) NOT NULL,
   `price` float NOT NULL,
   `description` varchar(1500) COLLATE utf8_unicode_ci NOT NULL,
-  `image_url` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `image_url` varchar(120) COLLATE utf8_unicode_ci NOT NULL,
   `is_sold` tinyint(1) NOT NULL COMMENT '1 if item is sold, else 0',
   `keywords` varchar(200) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -147,8 +167,9 @@ CREATE TABLE `items` (
 --
 
 INSERT INTO `items` (`item_id`, `price`, `description`, `image_url`, `is_sold`, `keywords`) VALUES
-(1, 54.88, 'This Acoustic Audio by Goldwood 2.1 speaker system Features a contemporary design, Bluetooth, Blue LED subwoofer and satellite lights and is a stylish addition to any home. This 3-piece system includes one powered subwoofer and two satellite speakers as well as instructions needed to \"plug and play\". use it for your personal computer or laptop, DVD player, TV, gaming system, MP3 player or other devices with RCA audio outputs (3.5mm to RCA cable included). the powered subwoofer Features a side-firing woofer and utilizes a digitally tuned wooden enclosure for increased bass response. The full-range satellite speakers feature magnetic shielding for use near Televisions and computer monitors.', '../img/51mg+MauKYL._SL1000_.jpg', 0, 'audio, music'),
-(2, 44.99, 'WD hard drives deliver solid performance and reliability while providing you with all the space you need to hold an enormous amount of photos, videos and files. These drives are designed for use as primary drives in desktops PCs, notebooks and external enclosures, and for certain industrial applications.', '../img/415198_622456_01_front_zoom.jpg', 0, 'electronic(other), gaming');
+(3, 120, 'The lightning-fast AMD Ryzen 7 processor is the superior choice. With AMD SenseMI technology, Ryzen processors use true machine intelligence to accelerate performance. All Ryzen 7 processors feature 8 cores with 16 threads for the most ambitious competitors, helping you game and stream at the same time with performance to spare.', '../uploads/63f6901cec179ba232a67ff5532f88b9.jpg', 0, 'electronic,all'),
+(4, 50, 'wd hard drive disk', '../uploads/0bda54c1816b41b9c72f4d32bf14ba8a.jpg', 0, 'electronic, all'),
+(5, 50, 'speakers that make sound', '../uploads/239435d356f1ac51f516cfe90849d8ce.jpg', 0, 'media, all');
 
 -- --------------------------------------------------------
 
@@ -173,13 +194,6 @@ CREATE TABLE `password_saver` (
   `user_fk_id` int(22) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Dumping data for table `password_saver`
---
-
-INSERT INTO `password_saver` (`session_id`, `time_stamp`, `user_fk_id`) VALUES
-('ac77ce5c25df91ea9b2b32bfe085f568cd5475c99ab280238f3c91c44f32e8b5', '2017-11-04 09:02:40', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -196,7 +210,11 @@ CREATE TABLE `phones` (
 --
 
 INSERT INTO `phones` (`phone_id`, `phone_number`) VALUES
-(1, '(651)-681-0684');
+(1, '(651)-681-0684'),
+(2, '(651)-303-9242'),
+(3, '(651)-472-3486'),
+(4, '(651)-472-3486'),
+(5, '(651)-303-9243');
 
 -- --------------------------------------------------------
 
@@ -222,7 +240,10 @@ CREATE TABLE `places` (
 
 INSERT INTO `places` (`place_id`, `street_number`, `route`, `locality`, `administrative_area_level_1`, `postal_code`, `country`, `lat`, `lng`) VALUES
 (6, '2016', 'Burnsville Center', 'Burnsville', 'MN', '55306', 'US', '44.7426029', '-93.2889313'),
-(12, '2016', 'Zircon Ln', 'Eagan', 'MN', '55122', 'US', '44.8053538', '-93.2095611');
+(12, '2016', 'Zircon Ln', 'Eagan', 'MN', '55122', 'US', '44.8053538', '-93.2095611'),
+(16, '2018', 'Zircon Ln', 'Eagan', 'MN', '55122', 'US', '44.8055751', '-93.2097063'),
+(17, '2090', 'Diffley Rd', 'Eagan', 'MN', '55122', 'US', '44.8044357', '-93.2142744'),
+(18, '10750', 'McCool Dr E', 'Burnsville', 'MN', '55337', 'US', '44.8094301', '-93.228979');
 
 -- --------------------------------------------------------
 
@@ -246,7 +267,8 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`user_id`, `username`, `password`, `fname`, `lname`, `email`) VALUES
 (1, 'id4766wa', '$2y$10$3Sk7watLoZ.KPxZIf3kwUOyVA/MUy4f8M9vq7k5ciSppgFI3ETJmO', 'gary', 'webb', 'id4766wa@metrostate.edu'),
 (2, 'yn0619nj', '$2y$10$dvRizOKMZ9o0PjkX7qODIOSWr2CiKDgPRnXFImeritw8oE3cJslua', 'david', 'gruenberg', 'yn0619nj@metrostate.edu'),
-(3, 'dd0517tz', '$2y$10$WaTGBmAXOjJ/3tc4S//Wu.I7bswVXvgyX5Z40/xih.wWcfgHi89GC', 'yeshewa', 'berhane', 'dd0517tz@metrostate.edu');
+(3, 'dd0517tz', '$2y$10$WaTGBmAXOjJ/3tc4S//Wu.I7bswVXvgyX5Z40/xih.wWcfgHi89GC', 'yeshewa', 'berhane', 'dd0517tz@metrostate.edu'),
+(4, 'Jumperv3', '$2y$10$0eCKASDIqDdi6LX/Q8RhfO5sMITRD6QO1aHlZCgNTS9NO8WgYq3v2', 'gary', 'webb', '4gwebb@gmail.com');
 
 --
 -- Indexes for dumped tables
@@ -342,7 +364,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `garage_sales`
 --
 ALTER TABLE `garage_sales`
-  MODIFY `gsale_id` int(22) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `gsale_id` int(22) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `invoice`
@@ -354,19 +376,25 @@ ALTER TABLE `invoice`
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `item_id` int(22) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `item_id` int(22) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `phones`
+--
+ALTER TABLE `phones`
+  MODIFY `phone_id` int(22) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `places`
 --
 ALTER TABLE `places`
-  MODIFY `place_id` int(22) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `place_id` int(22) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(22) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `user_id` int(22) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
@@ -376,14 +404,13 @@ ALTER TABLE `users`
 -- Constraints for table `garage_sales_items`
 --
 ALTER TABLE `garage_sales_items`
-  ADD CONSTRAINT `garage_sales_items_ibfk_1` FOREIGN KEY (`gsale_fk_id`) REFERENCES `garage_sales` (`gsale_id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `garage_sales_items_ibfk_2` FOREIGN KEY (`item_fk_id`) REFERENCES `items` (`item_id`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `garage_sales_items_ibfk_1` FOREIGN KEY (`gsale_fk_id`) REFERENCES `garage_sales` (`gsale_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `garage_sales_items_ibfk_2` FOREIGN KEY (`item_fk_id`) REFERENCES `items` (`item_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `garage_sales_phones`
 --
 ALTER TABLE `garage_sales_phones`
-  ADD CONSTRAINT `garage_sales_phones_ibfk_1` FOREIGN KEY (`phone_fk_id`) REFERENCES `phones` (`phone_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `garage_sales_phones_ibfk_2` FOREIGN KEY (`garage_sale_fk_id`) REFERENCES `garage_sales` (`gsale_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
