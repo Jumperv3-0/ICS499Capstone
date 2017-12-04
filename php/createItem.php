d400be85c812aa56302632b1ca1044bc00f21737
Jumperv3-0 <4gwebb@gmail.com>
2017-12-04 16:20:43 -0600

    create Item works

--- php/createItem.php
+++ php/createItem.php
@@ -6,6 +6,9 @@ $user = new User();
 if (!$user->isLoggedIn()) {
   Redirect::page('404.php');
 }
+//if (!$user->hasSale(sanitizeInput($_GET['gsale_id']))) {
+//  Redirect::page('404.php');
+//}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,6 +21,7 @@ if (!$user->isLoggedIn()) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="../css/styles.css">
   </head>
@@ -46,66 +50,67 @@ if (!$user->isLoggedIn()) {
       if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if (isset($_POST['submit']) && Token::check(sanitizeInput($_POST['token']))) {
           $validator = new validation();
+          $validator2 = new validation();
+          $rules2 = array(
+            'image' => array(
+              'image' => true,
+              'name' => "Image"
+            )
+          );
           $rules = array(
-            'item_name' => array(
-              'name' => "Item Name",
-              'required' => true,
-              'min' => 3,
-              'max' => 22
-            ),
             'description' => array(
               'max' => 10000, // FIXME: what is description max size?
-              'required' => false
+              'required' => false,
+              'name' => 'Description'
             ),
             'price' => array(
               'required' => true,
-              'price' => true
-            ));
+              'price' => true,
+              'name' => 'Price'
+            ),
+            'catagory' => array(
+              'required' => true,
+              'catagory' => true,
+              'name' => 'Catagory'
+            )
+          );
         }
+        $filename = "";
+        if (isset($_FILES['image']) && !empty($_FILES['image']['name'])) {
+          $filename = 'image';
+        } else {
+          $filename = null;
+        }
+        $image = new ImageProcesser($filename);
         $validation = $validator->check($_POST, $rules);
-        $image_url = "not_implented.png"; // TODO: need to upload image to downloads folder and create new url and save here
-        if ($validation->passed()) { //convert this to instead connect item to sale.
-          $user = new User();
-          $gsale = new GarageSale();
-          $place = new Place();
-          $formattedDates = formatDates();
+        $validation2 = $validator2->check($_FILES, $rules2);
+        if ($validation->passed() && $validation2->passed()) { //convert this to instead connect item to sale.
+          $item = new Item();
           try {
-            $address = $_POST['location'];
-            $address = sanitizeInput($_POST['location']);
-            $place->getPlaceJSON("?address=" . $address);
-            $address = sanitizeInput($address);
-            $place->create();
-            if ($place->exists()) {
-              var_dump($place);
-              // create garageSale()
-              $gsale->create(array(
-                sanitizeInput($_POST['sale_name']),
-                $image_url,
-                sanitizeInput($_POST['description']),
-                $formattedDates,
-                $place->getData()->place_id,
-                $user->data()->user_id
-              ));
-            }
+            echo "passed";
+            $params = array(sanitizeInput($_POST['price']), sanitizeInput($_POST['description']), $image->getNewName(), 0, (sanitizeInput($_POST['catagory'])) . ", all", sanitizeInput($_POST['gsale_id']));
+
+            //$params = array(price, description, image_url, 0, keywords);
+            $item->create($params);
+            Session::flash('addItem', "Your item was added!");
+            Redirect::page("yourSales.php");
           } catch (Exception $e) {
             die ($e->getMessage());
           }
         } else {
-          foreach ($validation->getErrors() as $error) {
-            echo $error . "<br>";
-          }
+          Session::flash('addItem', "Your item was not added!");
+          Redirect::page("yourSales.php");
+//          foreach ($validation->getErrors() as $error) {
+//            echo $error . "<br>";
+//          }
         }
         //Redirect::page("yourSales.php");
       }
       ?>
       <form action="<?php echo sanitizeInput($_SERVER['PHP_SELF']); ?>" method="post" enctype="multipart/form-data">
         <div class="form-group">
-          <label for="item_name">Name of Item:</label>
-          <input type="text" class="form-control" id="item_name" name="item_name" value="<?php echo(isset($_POST['item_name']) ? sanitizeInput($_POST['item_name']) : ''); ?>" placeholder="Enter name of item">
-        </div>
-        <div class="form-group">
           <label for="image">Image of Item (Optional):</label>
-          <input type="file" id="image" name="image">
+          <input type="file" id="image" name="image[]">
         </div>
         <div class="form-group">
           <label for="description">General Description:</label>
@@ -114,24 +119,24 @@ if (!$user->isLoggedIn()) {
 
         <div class="form-group">
           <label for="price">Price:</label>
-          <input class="form-control" type="text" id="price" placeholder="Enter price of item">
+          <input class="form-control" type="text" id="price" name="price" placeholder="Enter price of item">
         </div>
 
         <div class="form-group">
-              <label for="select">Change catagory:</label>
-              <select class="form-control" name="catagory[]" id="catagory">
-                <option value="default">---</option>
-                <option value="all" <?php  if (isset($_POST['catagory'])) {(strcmp(sanitizeInput($_POST['catagory']), 'all') ==0? 'selected' : '');} ?>>All Catagories</option>
-                <option value="clothes" <?php if (isset($_POST['catagory'])) {(strcmp(sanitizeInput($_POST['catagory']), 'clothes') ==0? 'selected' : '');} ?>>Clothes</option>
-                <option value="electronic" <?php if (isset($_POST['catagory'])) {(strcmp(sanitizeInput($_POST['catagory']), 'electronic') ==0? 'selected' : ''); }?>>Electronics</option>
-                <option value="furnture" <?php if (isset($_POST['catagory'])) { (strcmp(sanitizeInput($_POST['catagory']), 'furnture') ==0? 'selected' : '');} ?>>Furnture</option>
-                <option value="media" <?php if (isset($_POST['catagory'])) { (strcmp(sanitizeInput($_POST['catagory']), 'media') ==0? 'selected' : '');} ?>>Media eg.(Books, Magazines, Music, Movies)</option>
-                <option value="tool" <?php if (isset($_POST['catagory'])) { (strcmp(sanitizeInput($_POST['catagory']), 'tool') ==0? 'selected' : '');} ?>>Tools</option>
-                <option value="toy" <?php if (isset($_POST['catagory'])) { (strcmp(sanitizeInput($_POST['catagory']), 'toy') ==0? 'selected' : '');} ?>>Toys</option>
-                <option value="vehicle" <?php if (isset($_POST['catagory'])) { (strcmp(sanitizeInput($_POST['catagory']), 'vehicle') ==0? 'selected' : '');} ?>>Vehicle</option>
-                <option value="other" <?php if (isset($_POST['catagory'])) { (strcmp(sanitizeInput($_POST['catagory']), 'other') ==0? 'selected' : '');} ?>>Other</option>
-              </select>
-            </div>
+          <label for="select">Change catagory:</label>
+          <select class="form-control" name="catagory" id="catagory">
+            <option value="default">---</option>
+            <option value="clothes" <?php if (isset($_POST['catagory'])) {(strcmp(sanitizeInput($_POST['catagory']), 'clothes') ==0? 'selected' : '');} ?>>Clothes</option>
+            <option value="electronic" <?php if (isset($_POST['catagory'])) {(strcmp(sanitizeInput($_POST['catagory']), 'electronic') ==0? 'selected' : ''); }?>>Electronics</option>
+            <option value="furnture" <?php if (isset($_POST['catagory'])) { (strcmp(sanitizeInput($_POST['catagory']), 'furnture') ==0? 'selected' : '');} ?>>Furnture</option>
+            <option value="media" <?php if (isset($_POST['catagory'])) { (strcmp(sanitizeInput($_POST['catagory']), 'media') ==0? 'selected' : '');} ?>>Media eg.(Books, Magazines, Music, Movies)</option>
+            <option value="tool" <?php if (isset($_POST['catagory'])) { (strcmp(sanitizeInput($_POST['catagory']), 'tool') ==0? 'selected' : '');} ?>>Tools</option>
+            <option value="toy" <?php if (isset($_POST['catagory'])) { (strcmp(sanitizeInput($_POST['catagory']), 'toy') ==0? 'selected' : '');} ?>>Toys</option>
+            <option value="vehicle" <?php if (isset($_POST['catagory'])) { (strcmp(sanitizeInput($_POST['catagory']), 'vehicle') ==0? 'selected' : '');} ?>>Vehicle</option>
+            <option value="other" <?php if (isset($_POST['catagory'])) { (strcmp(sanitizeInput($_POST['catagory']), 'other') ==0? 'selected' : '');} ?>>Other</option>
+          </select>
+        </div>
+        <input type="hidden" name="gsale_id" value="<?php echo sanitizeInput($_GET['gsale_id']); ?>">
         <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
         <button type="submit" class="btn btn-default btn-green pull-right" name="submit">Submit</button>
       </form>
